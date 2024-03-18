52f2bc11b08f8612bcc39c6fb5e88a3701a2f37d
Ningjie Cao <kromatiks1@gmail.com>
2024-03-19 00:31:44 +0800

    arbi

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,6 @@
 class Service < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+  has_many :reviews, through: :bookings
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :services
+  has_many :bookings, through: :services # , as: :received_bookings
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
