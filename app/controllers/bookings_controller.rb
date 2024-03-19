dc3a8709f0ec741143378559f0ba82df23927981
jiahuizheng <ninhuijia@gmail.com>
2024-03-19 13:58:57 +0000

    booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,28 @@
 class BookingsController < ApplicationController
+  before_action :set_service, only: [:new, :create]
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user # getting the current user
+
+    if @booking.save
+      redirect_to @booking, notice: 'Booking was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_service
+    @service = Service.find(params[:service_id]) # pass service_id as a param
+  end
+
+  def booking_params
+    params.require(:booking).permit(:service_id, :start_date_time)
+  end
 end
