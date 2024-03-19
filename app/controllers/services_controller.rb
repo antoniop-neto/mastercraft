bb80cb467e008c7be3392a9aa35f891fb9827a3a
Antonio Oliveira <antoniop_neto@hotmail.com>
2024-03-19 17:04:03 +0000

    Generate the Services Controller and Actions

---
+++ app/controllers/services_controller.rb
@@ -0,0 +1,35 @@
+class ServicesController < ApplicationController
+  def index
+    @services = Service.all
+  end
+
+  def new
+    @service = Service.new
+  end
+
+  def create
+    @service = Service.new(service_params)
+    @service.user = current_user
+    if @service.save
+      redirect_to @service, notice: 'Service was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def manage
+    @user = current_user
+    @services = current_user.services
+  end
+
+  def destroy
+    @service = Service.find(params[:id])
+    @service.destroy
+  end
+
+  private
+
+  def service_params
+    params.require(:service).permit(:name, :price, :address)
+  end
+end
