487bff3dcdfb61a668e1ab2f12fc247ff2c80a4a
Ningjie Cao <kromatiks1@gmail.com>
2024-03-19 21:31:43 +0800

    seed file for testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :services
+  has_many :services, dependent: :destroy
   has_many :bookings, through: :services # , as: :received_bookings
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,23 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# clear the dabase
+Review.delete_all
+Booking.delete_all
+Service.delete_all
+User.delete_all
+
+puts "adding users"
+user1 = User.create!(email: "peter@gmail.com", first_name: "Peter", last_name: "Parker", password: "123456", password_confirmation: "123456")
+user2 = User.create!(email: "jane@gmail.com", first_name: "Jane", last_name: "Austen", password: "123456", password_confirmation: "123456")
+puts "adding services"
+service1 = Service.create!(user: user1, price: 50, name: "haircut", address: "10 Downing Street")
+service2 = Service.create!(user: user1, price: 100, name: "tattoo", address: "10 Downing Street")
+puts "adding bookings"
+booking1 = Booking.create!(user: user2, service:service1, start_date_time: Time.current, end_date_time: 1.hour.from_now)
+booking2 = Booking.create!(user: user2, service:service2, start_date_time: 2.hour.from_now, end_date_time: 3.hour.from_now)
+puts "adding reviews"
+review1 = Review.create!(booking: booking1, rating: 4.9, comment: "amaze")
+review2 = Review.create!(booking: booking2, rating: 5.0, comment: "perfection")
+puts "done"
