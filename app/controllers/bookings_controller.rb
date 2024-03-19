f806d4cfaa35146e9f07f44435f7311e19c939e0
Antonio Oliveira <antoniop_neto@hotmail.com>
2024-03-19 13:59:24 +0000

    adding edit, update and destroy actions to the bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,40 @@
 class BookingsController < ApplicationController
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.update(booking_params)
+  end
+
+  def destroy
+    @booking.Booking.find(params[:id])
+    @booking.destroy
+    #redirect_to bookings_path, status: :see_other
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date_time)
+  end
 end
