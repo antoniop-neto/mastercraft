77a3bb569370e1ea5e65bbe3d429cbab7cb16456
Ningjie Cao <kromatiks1@gmail.com>
2024-03-20 15:55:01 +0800

    modify

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   get "services/manage", to: "services#manage", as: :manage_services
-  resources :bookings, only: [:edit, :update, :destroy]
+  resources :bookings, only: [:index, :show, :edit, :update, :destroy]
 
 
   resources :services, only: [:index, :new, :create, :destroy] do
@@ -18,5 +18,5 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   # As a user, I want to be able to view the list of available services a specific provider provides
-  resources :users, only: [:show]
+  resources :users, only: [:index, :show]
 end
