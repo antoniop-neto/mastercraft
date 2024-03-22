0caa1875b9c340e9fb545429a773989ae394b83d
Antonio Oliveira <antoniop_neto@hotmail.com>
2024-03-22 13:52:37 +0000

    dependent changes

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :service
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   has_one :chatroom, dependent: :destroy
   after_create :create_private_chatroom
 
