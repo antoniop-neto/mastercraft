352bfb90d30f236371b5b0e0c61b010abb6aa838
Antonio Oliveira <antoniop_neto@hotmail.com>
2024-04-04 14:43:17 +0100

    skip authenticate_user for index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
   before_action :set_provider_boolean, :set_address, :set_dayslots, :set_average_rating
 
   def index
